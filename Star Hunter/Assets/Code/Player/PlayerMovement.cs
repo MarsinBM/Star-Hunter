c0ffa090c520853d3997ea57c5c734040b6a9532
Marcin W <A00283407@student.ait.ie>
2022-03-21 11:54:58 +0000

    started basic back and forward movement

--- Star Hunter/Assets/Code/Player/PlayerMovement.cs
+++ Star Hunter/Assets/Code/Player/PlayerMovement.cs
@@ -9,10 +9,11 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float speed;
     [SerializeField] Transform Tplayer;
     private float zRot;
+    private float xPush;
 
     void Start()
     {
-        
+        //Application.targetFrameRate = 15;
     }
 
     void Update()
@@ -20,22 +21,40 @@ public class PlayerMovement : MonoBehaviour
         movement();
     }
 
+    // Controls player movement
     void movement()
     {
+        // Movement Variables
         float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
         Vector2 turn = new Vector2(moveX, 0.0f);
+        Vector2 push = new Vector2(0.0f, moveY);
 
+        // Changes player rotation
         if (turn.x == 1)
         {
-            Debug.Log("pressed right");
-            zRot += -Time.deltaTime * turnspeed; // NEEDS TO BE CHECKED FOR FPS
+            //Debug.Log("pressed right");
+            zRot += -Time.deltaTime * turnspeed; 
         }
         if (turn.x == -1)
         {
-            Debug.Log("pressed left");
+            //Debug.Log("pressed left");
             zRot += Time.deltaTime * turnspeed;
         }
 
         Tplayer.rotation = Quaternion.Euler(0, 0, zRot);
+
+        // Moves player forward and backwards
+        if (push.y == 1)
+        {
+            Debug.Log("pressed up");
+            Tplayer.position += Vector3.forward * Time.deltaTime * speed;
+        }
+
+        if (push.y == -1)
+        {
+            Debug.Log("pressed down");
+            Tplayer.position -= Vector3.forward * Time.deltaTime * speed;
+        }
     }
 }
